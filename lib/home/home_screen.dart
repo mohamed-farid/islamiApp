71134b08c89911bc61bca2f6f15c77f2c3888366
mohamed-farid <59448168+mohamed-farid@users.noreply.github.com>
2024-11-15 18:08:46 +0200

    init project Islami

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,19 @@
+  import 'package:flutter/material.dart';
+
+  class HomeScreen extends StatelessWidget {
+    const HomeScreen({super.key});
+    static const String routeName = 'home_screen';
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'fasfasfas',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+      );
+    }
+  }
